ac05ee0dfd09312f0e0c9fe5f101999a66d1cc1a
Santos Larissa <larissa.santos@scania.com>
2022-08-22 23:21:01 -0300

    aula1

---
+++ estruturas/exemplo_condicinal.js
@@ -0,0 +1,7 @@
+const idade = 17;
+
+if (idade <= 18) {
+    console.log("Não Pode entrar na festa")
+} else {
+    console.log("Seja Bem Vindo")
+}
---
+++ modulo/app.js
@@ -0,0 +1,5 @@
+const m1 = require('./mod1')
+const m2 = require('./mod2')
+
+m1("Carregar uma unica função modular")
+m2.mensagem("Carrega objetos com funções modulares")
